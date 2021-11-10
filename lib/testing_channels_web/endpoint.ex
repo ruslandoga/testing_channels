393c86e1a9e1b7817651569d3e927273e225ebcd
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-11-10 18:30:40 +0300

    mix phx.gen.socket User

--- lib/testing_channels_web/endpoint.ex
+++ lib/testing_channels_web/endpoint.ex
@@ -10,6 +10,10 @@ defmodule TestingChannelsWeb.Endpoint do
     signing_salt: "OfXT4dt5"
   ]
 
+  socket "/socket", TestingChannelsWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
