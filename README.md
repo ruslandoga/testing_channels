469571dc39db434b42999f31060ccff6eb24b9ad
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-11-10 18:53:21 +0300

    remove unecessary stuff

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
+Getting:
+
 ```elixir
 > mix test
 
-....18:40:31.716 [error] Postgrex.Protocol (#PID<0.394.0>) disconnected: ** (DBConnection.ConnectionError) client #PID<0.473.0> exited
-..
+...18:54:04.486 [error] Postgrex.Protocol (#PID<0.417.0>) disconnected: ** (DBConnection.ConnectionError) client #PID<0.485.0> exited
 
-Finished in 0.06 seconds (0.06s async, 0.00s sync)
-6 tests, 0 failures
+
+Finished in 0.04 seconds (0.04s async, 0.00s sync)
+3 tests, 0 failures
+
+Randomized with seed 427296
+```
+
+Expecting:
+
+```elixir
+> mix test
+
+...18:54:04.486 [error] Postgrex.Protocol (#PID<0.417.0>) disconnected: ** (DBConnection.ConnectionError) client #PID<0.485.0> exited
+Client #PID<0.491.0> is still using a connection from owner at location:
+
+    ...stacktrace...
+
+The connection itself was checked out by #PID<0.491.0> at location:
+
+    ...stacktrace...
+
+
+Finished in 0.04 seconds (0.04s async, 0.00s sync)
+3 tests, 0 failures
+
+Randomized with seed 427296
 ```
--- test/testing_channels_web/channels/room_channel_test.exs
+++ test/testing_channels_web/channels/room_channel_test.exs
@@ -8,34 +8,14 @@ defmodule TestingChannelsWeb.RoomChannelTest do
   setup do
     {:ok, _, socket} =
       @socket
-      |> socket("user:1", %{some: :assign})
+      |> socket("user_socket:1", %{some: :assign})
       |> subscribe_and_join(TestingChannelsWeb.RoomChannel, "room:1")
 
-    {:ok, _, other_socket} =
-      @socket
-      |> socket("user:2", %{some: :assign})
-      |> subscribe_and_join(TestingChannelsWeb.RoomChannel, "room:2")
-
-    %{socket: socket, other_socket: other_socket}
-  end
-
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push(socket, "ping", %{"hello" => "there"})
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
-
-  test "shout broadcasts to room:lobby", %{socket: socket} do
-    push(socket, "shout", %{"hello" => "all"})
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
-
-  test "broadcasts are pushed to the client", %{socket: socket} do
-    broadcast_from!(socket, "broadcast", %{"some" => "data"})
-    assert_push "broadcast", %{"some" => "data"}
+    %{socket: socket}
   end
 
   test "repo use after test end" do
-    Phoenix.PubSub.broadcast(@pubsub, "user:2", {:event, 1 + 1})
+    Phoenix.PubSub.broadcast(@pubsub, "user:1", {:event, 1 + 1})
     Repo.query!("select 1 + 1")
   end
 end
